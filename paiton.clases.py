315a52ba7d68821b858178523b2c5b28299a06e3
Eduardo Escares <eduardoscrs60@gmail.com>
2023-10-18 11:16:56 -0300

    a

--- paiton.clases.py
+++ paiton.clases.py
@@ -1,66 +1,70 @@
 import tkinter as tk
 from tkinter import messagebox
 
-class NumberVisualizer:
+class Main_Ordenamiento:
     def __init__(self, window):
         self.window = window
         self.window.title("Ordenamiento y Búsqueda")
 
         self.lista = []
-        self.element_width = 20
+        # Ancho de cada elemento de la lista
+        self.element_width = 10
+        # Espacio entre cada elemento de la lista
         self.spacing = 5
 
+        # Crear el lienzo
         self.canvas = tk.Canvas(self.window, height=200)
+        # Empaquetar el lienzo
         self.canvas.pack()
 
-        self.create_input_widgets()
-        self.create_sorting_widgets()
-        self.create_search_widgets()
+        self.widget_entrada()
+        self.widget_ordenar()
+        self.widget_busqueda()
 
         self.window.protocol("WM_DELETE_WINDOW", self.window.quit)
     
     #* Función para crear los widgets de entrada
-    def create_input_widgets(self):
+    def widget_entrada(self):
         input_label = tk.Label(self.window, text="Ingrese números (separados por espacios):")
         input_label.pack()
 
-        self.input_entry = tk.Entry(self.window)
-        self.input_entry.pack()
+        self.input_entrada = tk.Entry(self.window)
+        self.input_entrada.pack()
 
-        add_button = tk.Button(self.window, text="Agregar números", command=self.add_numbers)
+        add_button = tk.Button(self.window, text="Agregar números", command=self.agregar_num)
         add_button.pack()
 
         clear_button = tk.Button(self.window, text="Limpiar Lista", command=self.clear_list)
         clear_button.pack()
         
     #* Función para crear los widgets de ordenamiento
-    def create_sorting_widgets(self):
-        sort_button = tk.Button(self.window, text="Iniciar Ordenamiento", command=self.start_sort)
+    def widget_ordenar(self):
+        sort_button = tk.Button(self.window, text="Iniciar Ordenamiento", command=self.ordernar)
         sort_button.pack()
         
     #* Función para crear los widgets de búsqueda
-    def create_search_widgets(self):
+    def widget_busqueda(self):
         search_label = tk.Label(self.window, text="Ingrese el número a buscar:")
         search_label.pack()
         # Entrada para la búsqueda binaria
         self.search_entry = tk.Entry(self.window)
         self.search_entry.pack()
         # Botón para realizar la búsqueda binaria
-        search_button = tk.Button(self.window, text="Buscar", command=self.binary_search)
+        search_button = tk.Button(self.window, text="Buscar", command=self.busq_bin)
         search_button.pack()
         # Etiqueta para mostrar el resultado de la búsqueda binaria
         self.search_result_label = tk.Label(self.window, text="")
         self.search_result_label.pack()
 
     #* Función para agregar los números ingresados por el usuario a la lista de números a ordenar y mostrarlos en el lienzo
-    def add_numbers(self):
-        user_input = self.input_entry.get()
-        if self.validate_input(user_input):
-            numbers = user_input.split()
-            for num in numbers:
+    def agregar_num(self):
+        input_usuario = self.input_entrada.get()
+        if self.validate_input(input_usuario):
+            numeros = input_usuario.split()
+            for num in numeros:
                 self.lista.append(int(num.strip()))
-            self.update_display()
-            self.input_entry.delete(0, "end")
+            self.actualizar_ventana()
+            self.input_entrada.delete(0, "end")
         else:
             tk.messagebox.showerror("Error", "Ingrese números enteros separados por espacios.")
 
@@ -70,16 +74,16 @@ class NumberVisualizer:
     
     def clear_list(self):
         self.lista = []
-        self.update_display()
+        self.actualizar_ventana()
    
 
     #* Función para iniciar el ordenamiento
-    def start_sort(self):
+    def ordernar(self):
         # Ordenar la lista usando el algoritmo de ordenamiento burbuja
         self.insertion_sort()
 
     #* Función para actualizar el lienzo
-    def update_display(self):
+    def actualizar_ventana(self):
         # Borrar todo lo que hay en el lienzo
         self.canvas.delete("all")
         # Dibujar los elementos de la lista
@@ -103,23 +107,23 @@ class NumberVisualizer:
             while j >= 0 and temp < self.lista[j]:
                 self.lista[j + 1] = self.lista[j]
                 j -= 1
-                self.update_display()
+                self.actualizar_ventana()
                 self.window.after(100)
             self.lista[j + 1] = temp
-        self.update_display()
+        self.actualizar_ventana()
 
     
     #* Función para realizar una búsqueda binaria con animación
-    def binary_search(self):
+    def busq_bin(self):
         # Obtener el valor a buscar
-        search_value = int(self.search_entry.get())
+        objetivo = int(self.search_entry.get())
         # Realizar la búsqueda binaria
-        result = self.busq_binaria_animacion(search_value)
+        result = self.busq_binaria_animacion(objetivo)
         # Mostrar el resultado de la búsqueda
         if result != -1:
-            self.search_result_label.config(text=f"El número {search_value} se encuentra en la posición {result}.")
+            self.search_result_label.config(text=f"El número {objetivo} se encuentra en la posición {result}.")
         else:
-            self.search_result_label.config(text=f"El número {search_value} no se encontró en la lista.")
+            self.search_result_label.config(text=f"El número {objetivo} no se encontró en la lista.")
 
     #* Función para realizar una búsqueda binaria con animación
     def busq_binaria_animacion(self, x):
@@ -133,9 +137,9 @@ class NumberVisualizer:
             # Destacar el elemento medio
             self.highlight_middle(medio)
             # Actualizar el lienzo
-            self.update_display()
+            self.actualizar_ventana()
             # Pequeña pausa para la animación
-            self.window.after(500)
+            self.window.after(150)
             # Si el elemento medio es el valor buscado, retornar el índice medio
             if self.lista[medio] == x:
                 self.clear_highlights()
@@ -153,7 +157,7 @@ class NumberVisualizer:
 
     def highlight_middle(self, medio):
         x = 20 + medio * (self.element_width + self.spacing)
-        self.canvas.create_rectangle(x, 150, x + self.element_width, 150 - self.lista[medio] * 5, fill="red")
+        self.canvas.create_rectangle(x, 150, x + self.element_width, 150 - self.lista[medio] * 5, fill="green")
         self.canvas.create_text(x + self.element_width / 2, 160, text=str(self.lista[medio]))
 
     def clear_highlights(self):
@@ -166,5 +170,5 @@ class NumberVisualizer:
 if __name__ == "__main__":
     window = tk.Tk()
     window.geometry("1000x500")
-    app = NumberVisualizer(window)
+    app = Main_Ordenamiento(window)
     window.mainloop()
--- paiton.py
+++ paiton.py
@@ -1,126 +1,20 @@
-import tkinter as tk
+def insertsort(lista):
+    for i in range(1, len(lista)):
+        temp = lista[i]
+        j = i - 1
+        while j >= 0 and temp < lista[j]:
+            lista[j + 1] = lista[j]
+            j -= 1
+        lista[j + 1] = temp
 
-# Función para agregar números a la lista y actualizar la visualización
-def add_numbers():
-    user_input = input_entry.get()
-    numbers = user_input.split()
-    for num in numbers:
-        if num.strip().isdigit():
-            lista.append(int(num.strip()))
-    update_display()
-    input_entry.delete(0, "end")
+        # Imprimir el estado de la lista en cada iteración
+        print(f"Iteración {i}: {lista}")
 
-# Función para iniciar el ordenamiento
-def start_sort():
-    bubble_sort(lista)
+    return lista
 
-# Función para actualizar la visualización
-def update_display():
-    canvas.delete("all")
-    num_elements = len(lista)
-    element_width = 20
-    spacing = 5  # Espacio entre los elementos
-    canvas.config(width=num_elements * (element_width + spacing) + 20)  # Ajustar el ancho del lienzo
-    x = 20
-    for num in lista:
-        canvas.create_rectangle(x, 150, x + element_width, 150 - num * 5, fill="blue")
-        canvas.create_text(x + element_width / 2, 160, text=str(num))
-        x += element_width + spacing
-    window.update()
 
-# Función para ordenar con el algoritmo de burbuja
-def bubble_sort(lista):
-    n = len(lista)
-    for i in range(n):
-        for j in range(0, n-i-1):
-            if lista[j] > lista[j+1]:
-                lista[j], lista[j+1] = lista[j+1], lista[j]
-                update_display()
-                window.after(100)  # Pequeña pausa para visualización
+lista = [1, 5, 3, 2, 4, 3, 4, 5, 12, 11, 10, 9, 8, 7, 6]
 
-# Función para buscar con el algoritmo de búsqueda binaria con animación
-def binary_search():
-    search_value = int(search_entry.get())
-    result = busq_binaria_animacion(lista, search_value)
-    if result != -1:
-        search_result_label.config(text=f"El número {search_value} se encuentra en la posición {result}.")
-    else:
-        search_result_label.config(text=f"El número {search_value} no se encontró en la lista.")
-
-# Función para realizar una búsqueda binaria con animación
-def busq_binaria_animacion(lista, x):
-    izq = 0
-    der = len(lista) - 1
-    while izq <= der:
-        medio = (izq + der) // 2
-        highlight_middle(medio)  # Destacar el elemento medio
-        update_display()
-        window.after(500)  # Pequeña pausa para la animación
-        if lista[medio] == x:
-            clear_highlights()
-            return medio
-        elif lista[medio] > x:
-            clear_highlights()
-            der = medio - 1
-        else:
-            clear_highlights()
-            izq = medio + 1
-    clear_highlights()
-    return -1
-
-# Función para destacar el elemento medio
-def highlight_middle(medio):
-    x = 20 + medio * (element_width + spacing)
-    canvas.create_rectangle(x, 150, x + element_width, 150 - lista[medio] * 5, fill="red")
-    canvas.create_text(x + element_width / 2, 160, text=str(lista[medio]))
-
-# Función para borrar todos los resaltados
-def clear_highlights():
-    canvas.delete("red")
-
-# Crear una ventana
-window = tk.Tk()
-window.title("Ordenamiento y Búsqueda")
-
-# Crear un lienzo para dibujar
-canvas = tk.Canvas(window, height=200)
-canvas.pack()
-
-# Crear una entrada para que el usuario ingrese números
-input_label = tk.Label(window, text="Ingrese números (separados por espacios):")
-input_label.pack()
-input_entry = tk.Entry(window)
-input_entry.pack()
-
-# Botón para agregar los números
-add_button = tk.Button(window, text="Agregar números", command=add_numbers)
-add_button.pack()
-
-# Botón para iniciar el ordenamiento
-sort_button = tk.Button(window, text="Iniciar Ordenamiento", command=start_sort)
-sort_button.pack()
-
-# Entrada para la búsqueda binaria
-search_label = tk.Label(window, text="Ingrese el número a buscar:")
-search_label.pack()
-search_entry = tk.Entry(window)
-search_entry.pack()
-
-# Botón para realizar la búsqueda binaria
-search_button = tk.Button(window, text="Buscar", command=binary_search)
-search_button.pack()
-
-# Etiqueta para mostrar el resultado de la búsqueda binaria
-search_result_label = tk.Label(window, text="")
-search_result_label.pack()
-
-# Lista de números
-lista = []
-element_width = 20
-spacing = 5  # Espacio entre los elementos
-
-# Cerrar la ventana al hacer clic en la "X"
-window.protocol("WM_DELETE_WINDOW", window.quit)
-
-# Ejecutar la ventana
-window.mainloop()
+print(insertsort(lista))                
+                
+                
